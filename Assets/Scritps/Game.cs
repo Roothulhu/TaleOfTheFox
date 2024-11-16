e8eb57bb2bc1f5fd15a9b283d46f4918ff3bc8f7
Rafael Hernandez Chavez <64990211+RafaHdzCh@users.noreply.github.com>
2024-11-16 11:15:39 -0600

    Canvas and ui

--- Assets/Scritps/Game.cs
+++ Assets/Scritps/Game.cs
@@ -2,5 +2,25 @@ using UnityEngine;
 
 public class Game : MonoBehaviour
 {
+    [SerializeField] private GameObject misionGO;
+
+    void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.M))
+            {
+                ToggleMission();
+            }
+        }
     
+    void ToggleMission()
+    {
+        if (misionGO.activeInHierarchy)
+        {
+            misionGO.SetActive(false);
+        }
+        else
+        {
+            misionGO.SetActive(true);
+        }
+    }
 }
