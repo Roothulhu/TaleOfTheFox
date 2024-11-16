0dc6cbf1d4c082b3302d5be134e7b0bd5533e6d8
Rafael Hernandez Chavez <64990211+RafaHdzCh@users.noreply.github.com>
2024-11-16 11:18:50 -0600

    Finish canvas

--- Assets/Scritps/Finish.cs
+++ Assets/Scritps/Finish.cs
@@ -1,16 +1,10 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Finish : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void ChangeScene(int scene)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(scene);
     }
 }
